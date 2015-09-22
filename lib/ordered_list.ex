f1853ca58949bdf73b04b32a152d7168cf5d0c11
Aaron Dufall <aald212@gmail.com>
2015-09-22 12:31:49 -0700

    add move up and move down with helpers

--- lib/ordered_list.ex
+++ lib/ordered_list.ex
@@ -1,33 +1,53 @@
 defmodule OrderedList do
 
   def insert_at(list, element, new_position) do
-    list
-      |> Enum.filter(&(&1 != element))
-      |> Enum.sort_by(&(&1.position))
-      |> Enum.partition(&(&1.position < new_position))
-      |> reposition(element,new_position)      
+    if valid_position?(list,element,new_position) do
+      { reorder, ordered } = Enum.partition(list, &(&1.position in element.position..new_position))
+      reorder = Enum.filter(reorder, &(&1.position != element.position))
+
+      reorder = 
+        case element.position > new_position do 
+          true -> increment_positions(reorder)
+          false -> decrement_positions(reorder)
+        end
+
+      element = Map.put(element, :position, new_position)
+
+      Enum.sort_by(reorder ++ ordered ++ [element], &(&1.position))
+    else
+      list
+    end    
+  end
+
+  def move_lower(list, element) do
+    insert_at(list,element, element.position + 1)
+  end
+
+  def move_higher(list, element) do
+    insert_at(list,element, element.position - 1)
+  end
+
+  def first?(_, element) do
+    element.position == 1
+  end
+
+  def last?(list, element) do
+    Enum.max_by(list, &(&1.position)).position == element.position
   end
 
   #private
 
-  defp reposition({before, [rest]}, element, new_position) do
-    decrement_positions(before ++ [rest], element.position)
-    ++ [Map.put(element, :position, new_position)] 
+  defp valid_position?(list, element, new_position) do
+    element.position != new_position && 
+    !(first?(list, element) && new_position < 1) &&
+    !(last?(list, element) && new_position > element.position)
   end
 
-  defp reposition({before, rest}, element, new_position) do
-    before
-    ++ [Map.put(element, :position, new_position)] 
-    ++ increment_positions(rest, element.position)
+  defp increment_positions(list) do
+    Enum.map(list, fn(list_item) -> move_down(list_item) end)
   end
 
-  defp increment_positions(list, stop) do
-    {increment, leave} = Enum.partition(list, &(&1.position < stop))
-    Enum.map(increment, fn(list_item) -> move_down(list_item) end) ++ leave
-  end
-
-  defp decrement_positions(list, stop) do
-    {decrement, _ } = Enum.partition(list, &(&1.position > stop))
+  defp decrement_positions(list) do
     Enum.map(list, fn(list_item) -> move_up(list_item) end)
   end
 
--- test/ordered_list_test.exs
+++ test/ordered_list_test.exs
@@ -41,4 +41,117 @@ defmodule OrderedListTest do
         %{id: 1, position: 5} 
       ]
   end
+
+  #move_lower/2
+
+  test "#move_lower/2" do 
+    start_list = [
+      %{id: 1, position: 1}, 
+      %{id: 2, position: 2}, 
+      %{id: 3, position: 3}, 
+      %{id: 4, position: 4},
+      %{id: 5, position: 5}
+    ]
+
+    # Top
+    assert OrderedList.move_lower(start_list, %{id: 1, position: 1}) ==
+      [
+        %{id: 2, position: 1}, 
+        %{id: 1, position: 2}, 
+        %{id: 3, position: 3}, 
+        %{id: 4, position: 4},
+        %{id: 5, position: 5}
+      ]
+    # Middle
+    assert OrderedList.move_lower(start_list, %{id: 3, position: 3}) ==
+      [
+        %{id: 1, position: 1}, 
+        %{id: 2, position: 2}, 
+        %{id: 4, position: 3},
+        %{id: 3, position: 4}, 
+        %{id: 5, position: 5}
+      ]
+
+    # Bottom
+    assert OrderedList.move_lower(start_list, %{id: 5, position: 5}) ==
+      [
+        %{id: 1, position: 1}, 
+        %{id: 2, position: 2}, 
+        %{id: 3, position: 3}, 
+        %{id: 4, position: 4},
+        %{id: 5, position: 5}
+      ]
+
+  end
+
+  #move_higher/2
+  
+  test "#move_higher/2" do 
+    start_list = [
+      %{id: 1, position: 1}, 
+      %{id: 2, position: 2}, 
+      %{id: 3, position: 3}, 
+      %{id: 4, position: 4},
+      %{id: 5, position: 5}
+    ]
+
+    # Top
+    assert OrderedList.move_higher(start_list, %{id: 1, position: 1}) ==
+      [
+        %{id: 1, position: 1}, 
+        %{id: 2, position: 2}, 
+        %{id: 3, position: 3}, 
+        %{id: 4, position: 4},
+        %{id: 5, position: 5}
+      ]
+    # Middle
+    assert OrderedList.move_higher(start_list, %{id: 3, position: 3}) ==
+      [
+        %{id: 1, position: 1}, 
+        %{id: 3, position: 2}, 
+        %{id: 2, position: 3}, 
+        %{id: 4, position: 4},
+        %{id: 5, position: 5}
+      ]
+
+    # Bottom
+    assert OrderedList.move_higher(start_list, %{id: 5, position: 5}) ==
+      [
+        %{id: 1, position: 1}, 
+        %{id: 2, position: 2}, 
+        %{id: 3, position: 3}, 
+        %{id: 5, position: 4},
+        %{id: 4, position: 5}
+      ]
+
+  end
+
+  #first?/2
+
+  test "first?/2" do
+    start_list = [
+      %{id: 1, position: 1}, 
+      %{id: 2, position: 2}, 
+      %{id: 3, position: 3}, 
+      %{id: 4, position: 4},
+      %{id: 5, position: 5}
+    ]
+    assert OrderedList.first?(start_list, %{id: 1, position: 1}) == true
+    refute OrderedList.first?(start_list, %{id: 2, position: 2}) == true
+  end
+
+  #last?/2
+
+  test "first?/2" do
+    start_list = [
+      %{id: 1, position: 1}, 
+      %{id: 2, position: 2}, 
+      %{id: 3, position: 3}, 
+      %{id: 4, position: 4},
+      %{id: 5, position: 5}
+    ]
+    assert OrderedList.last?(start_list, %{id: 5, position: 5}) == true
+    refute OrderedList.last?(start_list, %{id: 3, position: 3}) == true
+  end
+
 end
