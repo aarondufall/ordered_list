2958a8eb2829d67582e07bda9a0a5e5828366702
Aaron Dufall <aald212@gmail.com>
2015-09-26 17:02:15 -0700

    Add relase docs and config

--- mix.exs
+++ mix.exs
@@ -5,7 +5,12 @@ defmodule OrderedList.Mixfile do
     [app: :ordered_list,
      version: "0.1.0",
      elixir: "~> 1.0",
-     deps: deps]
+     deps: deps,
+     source_url: "https://github.com/aarondufall/ordered_list",
+     # Hex
+     description: description,
+     package: package,
+    ]
   end
 
   # Configuration for the OTP application
@@ -26,5 +31,18 @@ defmodule OrderedList.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     [{:inch_ex, only: :docs}]
+  end
+
+  defp description do
+    """
+    Sorting and reordering positions in a list.
+    """
+  end
+
+  defp package do
+    [contributors: ["Aaron Dufall"],
+     licenses: ["MIT"],
+     links: %{github: "https://github.com/aarondufall/ordered_list"},
+     files: ~w(mix.exs README.md LICENSE lib)]
   end
 end
